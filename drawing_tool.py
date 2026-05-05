b7ff57a6bc73fe3054b59a8fab47ee8d4f089007
mmdark <fhock2002@gmail.com>
2026-05-05 14:22:39 +0200

    fix start drawing line lagging

--- drawing_tool.py
+++ drawing_tool.py
@@ -26,39 +26,43 @@ class State:
 
     def update_scale_offset(self):
         if self.image_width > 0 and self.image_height > 0:
-            self.scale = min(self.width/self.image_width, self.height/self.image_height)
-            self.offset_x = (self.width - (self.image_width * self.scale))/2
-            self.offset_y = (self.height - (self.image_height * self.scale))/2
-    
+            self.scale = min(self.width / self.image_width, self.height / self.image_height)
+            self.offset_x = (self.width - (self.image_width * self.scale)) / 2
+            self.offset_y = (self.height - (self.image_height * self.scale)) / 2
+
+
 class DrawingTool(cv.Canvas):
-    def __init__(self, on_cell_drawn: typing.Callable[[list], None] = None, on_cell_deleted: typing.Callable[[tuple], None] = None,on_show_ids: typing.Callable[[tuple], None] = None):
+    def __init__(self, on_cell_drawn: typing.Callable[[list], None] = None,
+                 on_cell_deleted: typing.Callable[[tuple], None] = None,
+                 on_show_ids: typing.Callable[[tuple], None] = None):
         super().__init__()
-        self.draw_color = rgb_to_hex((0,255,0))
-        self.left=0
-        self.right=0
-        self.top=0
-        self.bottom=0
+        self.draw_color = rgb_to_hex((0, 255, 0))
+        self.left = 0
+        self.right = 0
+        self.top = 0
+        self.bottom = 0
         self.expand = False
         self.on_cell_drawn = on_cell_drawn
         self.on_cell_deleted = on_cell_deleted
-        self.show_ids =on_show_ids
+        self.show_ids = on_show_ids
         self._state = State()
         self.content = ft.GestureDetector(
-            on_tap_down= self.handle_click,
+            on_tap_down=self.handle_click,
             on_pan_start=self.handle_pan_start,
             on_pan_update=self.handle_pan_update,
             on_pan_end=self.handle_pan_end,
-            on_hover = self.handle_hover,
-            drag_interval=0,
+            on_hover=self.handle_hover,
+            drag_interval=5,
+            hover_interval=25,
         )
-        self.on_resize=self.on_canvas_resize
+        self.on_resize = self.on_canvas_resize
 
     def on_canvas_resize(self, e: cv.CanvasResizeEvent):
         self._state.width = e.width
         self._state.height = e.height
         self._state.update_scale_offset()
 
-    def set_bounds(self,width,height):
+    def set_bounds(self, width, height):
         self._state.image_width = width
         self._state.image_height = height
         self._state.update_scale_offset()
@@ -86,12 +90,11 @@ class DrawingTool(cv.Canvas):
     def deactivate_cell_info(self):
         self._state.cell_info_mode = False
 
-
     async def handle_click(self, e: ft.TapEvent):
         if self._state.delete_mode:
             self.on_cell_deleted(self.translate_into_image_coordinates((e.local_position.x, e.local_position.y)))
 
-    async def handle_hover (self, e:ft.HoverEvent):
+    async def handle_hover(self, e: ft.HoverEvent):
         if self._state.cell_info_mode and self._state.start_point is None:
             self.show_ids(self.translate_into_image_coordinates((e.local_position.x, e.local_position.y)))
 
@@ -103,21 +106,22 @@ class DrawingTool(cv.Canvas):
             if self._state.start_point is None:
                 self._state.start_point = (x, y)
 
-
     async def handle_pan_update(self, e: ft.DragUpdateEvent):
         if self._state.drawing_mode:
             ft.context.disable_auto_update()
-            x,y = self.clamp_to_image_bounds((e.local_position.x, e.local_position.y))
+            x, y = self.clamp_to_image_bounds((e.local_position.x, e.local_position.y))
             self.shapes.append(
                 cv.Line(
                     x1=self._state.x,
                     y1=self._state.y,
                     x2=x,
                     y2=y,
-                    paint=ft.Paint(stroke_width=3,color=self.draw_color),
+                    paint=ft.Paint(stroke_width=3, color=self.draw_color, style=ft.PaintingStyle.STROKE,
+                                   stroke_cap=ft.StrokeCap.ROUND, stroke_join=ft.StrokeJoin.ROUND),
                 )
             )
-            self._state.lines.append((self.translate_into_image_coordinates((self._state.x, self._state.y)), self.translate_into_image_coordinates((x, y))))
+            self._state.lines.append((self.translate_into_image_coordinates((self._state.x, self._state.y)),
+                                      self.translate_into_image_coordinates((x, y))))
             self.update()
 
             self._state.x = x
@@ -125,7 +129,9 @@ class DrawingTool(cv.Canvas):
 
     async def handle_pan_end(self):
         if self._state.drawing_mode and self._state.start_point is not None:
-            self._state.lines.append((self.translate_into_image_coordinates((self._state.x, self._state.y)), self.translate_into_image_coordinates((self._state.start_point[0], self._state.start_point[1]))))
+            self._state.lines.append((self.translate_into_image_coordinates((self._state.x, self._state.y)),
+                                      self.translate_into_image_coordinates(
+                                          (self._state.start_point[0], self._state.start_point[1]))))
             self.on_cell_drawn(self._state.lines)
             self._state.lines.clear()
             self.shapes.clear()
